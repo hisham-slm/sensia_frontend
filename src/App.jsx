de0c32f39bbaa5a9081d1d93215478e125728d43
hisham-slm <hishamsalam350@gmail.com>
2025-05-28 14:59:09 +0400

    add auth each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
+import { CookiesProvider, useCookies } from "react-cookie";
 import { Toaster } from "react-hot-toast";
 
 import AdminLogin from "./pages/AdminLogin.jsx";
@@ -9,28 +9,31 @@ import AdminDashboard from "./pages/AdminPage.jsx";
 import AddUser from "./pages/AddUser.jsx";
 import "./index.css";
 
+function AppRoutes() {
+  const [cookies] = useCookies(['access_token']);
+  const isAuthenticated = !!cookies.access_token;
+
+  return (
+    <Routes>
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route
+        path="/admin/home"
+        element={
+          isAuthenticated ? <AdminDashboard /> : <Navigate to="/admin/login" replace />
+        }
+      />
+      <Route path="/user/login" element={<UserLogin />} />
+      <Route path="/user/add" element={<AddUser />} />
+    </Routes>
+  );
+}
+
 function App() {
-  const isAuthenticated = false; // update this based on your cookie/token logic
   return (
     <CookiesProvider>
       <Toaster />
       <Router>
-        <Routes>
-          <Route
-            path="/admin/login"
-            element={
-              <AdminLogin />
-            }
-          />
-          <Route
-            path="/admin/home"
-            element={
-              !isAuthenticated ? <AdminDashboard /> : <Navigate to="/admin/login" replace />
-            }
-          />
-          <Route path="/user/login" element={<UserLogin />} />
-          <Route path="/user/add" element={<AddUser />} />
-        </Routes>
+        <AppRoutes />
       </Router>
     </CookiesProvider>
   );
